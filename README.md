8d17e67eccb68fcb0a2881b213fc08bcaa9dd80e
SinekaRamu <sineka2001cud@gmail.com>
2023-12-01 20:01:31 +0530

    recipe-app

---
+++ README.md
@@ -0,0 +1,21 @@
+# Recipe App
+
+Recipe App contains a form and card of the recipe. Form have a input element for title block, prepration time, steps for the recipe, image of the recipe and button to add the recipe as a card.
+
+## Form Submit
+
+Display the form input data in the console, while clicking submit
+
+## create card using form
+
+added card using form input content and sumbit button
+
+## Added time split and total card count
+
+```
+t = recipe["time"].split(":");
+  min.innerText = t[0] + "hrs" + t[1] + "mins";
+  time.append(labelTt, min);
+```
+
+Using HTML, CSS, Javascript, we created a form to add new receipe steps and the added information are saved in local storage.
---
+++ app/script.js
@@ -0,0 +1,134 @@
+//Inialize the global vaiable
+const cardBody = document.querySelector("#display-card");
+
+let recipeItems = [];
+
+const recipeCount = document.querySelector("#count");
+
+// Main function
+updateForm();
+getFromLocalSorage();
+
+function updateForm() {
+  const form = document.querySelector("#recipe-form");
+  form.addEventListener("submit", function (r) {
+    r.preventDefault();
+    updateFormElements();
+  });
+}
+
+function updateFormElements() {
+  // fetch form data
+  const title = document.querySelector("#recipeName").value;
+  const time = document.querySelector("#timeTaken").value;
+  const steps = document.querySelector("#stepsTodo").value;
+  const image = document.querySelector("#recipeImg").value;
+
+  const recipe = {
+    id: new Date().getTime(),
+    title: title,
+    time: time,
+    steps: steps,
+    image: image,
+  };
+  addRecipe(recipe);
+  document.querySelector("#recipe-form").reset();
+}
+
+//function to add recipe
+function addRecipe(recipe) {
+  recipeItems.push(recipe);
+  setToLocalStorage();
+}
+
+// function to save data in local storage
+function setToLocalStorage() {
+  const str = JSON.stringify(recipeItems);
+  localStorage.setItem("Recipes", str);
+  updateUi();
+}
+
+// Function to get data from localStorage
+function getFromLocalSorage() {
+  const str = localStorage.getItem("Recipes");
+  if (!str) {
+    recipeItems = [];
+  } else {
+    recipeItems = JSON.parse(str);
+  }
+  updateUi();
+}
+
+function updateUi() {
+  clearApp();
+  for (let i = 0; i < recipeItems.length; i++) {
+    const recipeDiv = displayCard(recipeItems[i]);
+    cardBody.appendChild(recipeDiv);
+    recipeCount.innerText = recipeItems.length;
+  }
+}
+
+//function to clear the UI
+function clearApp() {
+  cardBody.innerHTML = "";
+}
+
+updateUi();
+// creating card using HTML tags
+function displayCard(recipe) {
+  const cardBox = document.createElement("div");
+  cardBox.setAttribute("class", "recipe-card");
+  const id = `item-${recipe["id"]}`;
+  cardBox.setAttribute("id", id);
+
+  const imageBox = document.createElement("div");
+  imageBox.setAttribute("class", "imageBox");
+
+  const img = document.createElement("img");
+  img.setAttribute("class", "img");
+  img.src = recipe["image"];
+  imageBox.append(img);
+
+  const contentBox = document.createElement("div");
+  contentBox.setAttribute("class", "contentBox");
+  cardBox.append(imageBox, contentBox);
+
+  const headerBox = document.createElement("div"); //flex header division
+  headerBox.setAttribute("class", "flex-box");
+  const title = document.createElement("h2");
+  title.innerText = recipe["title"];
+  const time = document.createElement("div"); //flex header division
+  time.setAttribute("class", "flex");
+  headerBox.append(title, time);
+
+  const labelTt = document.createElement("h3");
+  labelTt.innerText = "Time Taken :";
+  const min = document.createElement("time");
+  t = recipe["time"].split(":");
+  min.innerText = t[0] + "hrs" + t[1] + "mins";
+  time.append(labelTt, min);
+
+  const stepsBox = document.createElement("div"); //flex header division
+  const labelSt = document.createElement("h3");
+  labelSt.innerText = "Steps Todo :";
+  const steps = document.createElement("p");
+  steps.setAttribute("class", "stepsTodo");
+  steps.innerText = recipe["steps"];
+  stepsBox.append(labelSt, steps);
+
+  const DeleteBtn = document.createElement("button"); //button to delete card
+  DeleteBtn.textContent = "delete";
+  DeleteBtn.addEventListener("click", function () {
+    remove(recipe["id"]);
+  });
+  contentBox.append(headerBox, stepsBox, DeleteBtn);
+
+  return cardBox;
+}
+
+//Function to remove
+function remove(recipeId) {
+  const filterArrray = recipeItems.filter((recipe) => recipe.id != recipeId);
+  recipeItems = filterArrray;
+  setToLocalStorage();
+}
